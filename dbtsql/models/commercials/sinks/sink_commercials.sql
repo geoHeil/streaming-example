475bfd9f0b8bafe34a0dfdf735d0a0b3b06a717e
Georg Heiler <georg.kf.heiler@gmail.com>
2022-03-25 11:39:07 +0100

    immutable sink

--- dbtsql/models/commercials/sinks/sink_commercials.sql
+++ dbtsql/models/commercials/sinks/sink_commercials.sql
@@ -10,5 +10,10 @@ CREATE SINK {{ sink_name }}
 FROM {{ ref('fct_commercials') }}
 INTO KAFKA BROKER 'localhost:9092' TOPIC 'metrics_per_brand_materialize'
 KEY (brand)
+
+CONSISTENCY (TOPIC 'metrics_per_brand_materialize-consistency'
+             FORMAT AVRO USING CONFLUENT SCHEMA REGISTRY 'http://localhost:8081')
+WITH (reuse_topic=true)
+
 FORMAT AVRO USING
     CONFLUENT SCHEMA REGISTRY 'http://localhost:8081';
